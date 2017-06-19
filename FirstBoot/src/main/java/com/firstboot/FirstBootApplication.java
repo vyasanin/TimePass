01c22fc779d90b05c93c00da9eb7cb65a2e9a50e
vyasanin <reddy.kasireddy@gmail.com>
2017-06-18 23:53:13 -0500

    Initial Commit for firstboot

---
+++ FirstBoot/src/main/java/com/firstboot/FirstBootApplication.java
@@ -0,0 +1,12 @@
+package com.firstboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstBootApplication.class, args);
+	}
+}
