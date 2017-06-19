6b3d0f5964d486811b2f5ae3d62e1909fc4e731e
vyasanin <reddy.kasireddy@gmail.com>
2017-06-19 00:11:06 -0500

    first success hello world

---
+++ FirstBoot/src/main/java/com/firstboot/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.firstboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+
+	@RequestMapping("/")
+	public String sayHello()
+	{
+		return "index";
+	}
+}
